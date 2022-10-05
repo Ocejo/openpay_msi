52d2f8010883e6cd6d0f7633d09d2b5ca44a6001
Ocejo <ocejojimenezjorgeluis@gmail.com>
2022-10-04 22:32:35 -0500

    primera entrega

--- app.php
+++ app.php
@@ -6,30 +6,39 @@ $data = json_decode(file_get_contents('php://input'), true);
 try {
     $openpay  = Openpay::getInstance('mmey69zmnv0ub7zhny23', 'sk_1dc6b7d82dd24b8bac53bfaad6f9ec70');
     $customer = array(
-        'name'         => 'Juan',
-        'last_name'    => 'Vazquez Juarez',
-        'phone_number' => '4423456723',
-        'email'        => 'juan.vazquez@empresa.com.mx'
+        'name'         => $data['customer']['name'],
+        'last_name'    => $data['customer']['lastName'],
+        'phone_number' => $data['customer']['phone'],
+        'email'        => $data['customer']['email']
     );
-    $payments = array('payments' => 6);
+    $payments      = array('payments' => $data['plan']);
+    $security      = array('use_3d_secure' => $data['security'], 'redirect_url' => 'http://localhost:8888/openpay_msi/success.html');
     $chargeRequest = array(
         'method'            => 'card',
         'source_id'         => $data['token'],
-        'amount'            => 3500,
+        'amount'            => $data['mount'],
         'currency'          => 'MXN',
         'description'       => 'Cargo inicial a mi merchant',
         'device_session_id' => $data['device_session_id'],
         'payment_plan'      => $payments,
-        'customer'          => $customer);
-
+        'customer'          => $customer,
+        'use_3d_secure'     => $data['security'],
+        'redirect_url'      => 'http://localhost:8888/openpay_msi/success.html'
+    );
     $charge = $openpay->charges->create($chargeRequest);
-    if ($charge->status=='completed') {
-        echo json_encode(['success' => true, 'message' => 'Pago completado']);
-    }else{
+    if ($charge->status == 'completed') {
+        echo json_encode(['success' => true, 'message' => 'Pago completado', 'url' => '']);
+    } else if ($charge->status == 'charge_pending') {
+        echo json_encode(['success' => true, 'message' => 'Te dirigiremos para que ingreses tu clave', 'url' => $charge->payment_method->url]);
+    } else {
         echo json_encode(['success' => false, 'message' => $charge->description]);
     }
-    var_dump($charge->status);
 } catch (\Throwable$th) {
     echo '<pre>';
     die(var_dump($th));
+    foreach (json_decode($th) as $key => $value) {
+        echo '<pre>';
+        die(var_dump($value));
+    }
+    // echo json_encode(['success' => false, 'message' => $th]);
 }
--- js/main.js
+++ js/main.js
@@ -7,12 +7,29 @@ var app = new Vue({
     expDateYear: "",
     csv: "",
     plan: 0,
-    email:'',
-    phone:'',
+    isDisabled: false,
+    btnText:'Pagar',
+    product:[],
+    productText:'none',
+    mountText:'$0',
+    mount:0,
+    plan:'',
+    plans:[],
+    customer: {
+      name: "",
+      lastName:"",
+      email: "",
+      phone: "",
+    }
   },
   methods: {
     payment: function () {
-      this.valideteForm();
+      this.btnPagar('p');
+      if(!this.valideteForm()){
+        this.btnPagar('c');
+        Swal.fire("Error", 'Campos Requeridos', "error");
+        return true;
+      }
       OpenPay.token.create(
         {
           card_number: this.numCard,
@@ -25,28 +42,98 @@ var app = new Vue({
         this.onError
       );
     },
-    onSuccess : function(response){
+    onSuccess: function (response) {
+      this.btnPagar('p');
+      const that = this;
       let post_data = {
         token: response.data.id,
-        device_session_id: $('#deviceIdHiddenFieldName').val()
+        device_session_id: $("#deviceIdHiddenFieldName").val(),
+        plan: that.plan,
+        mount: that.mount,
+        customer: that.customer,
+        security: (this.mount > 5000) ? true : false,
       };
-      this.$http.post('./app.php',JSON.stringify(post_data)).then(
-        function (response){
-          console.log(response.body);
+      this.$http.post("./app.php", JSON.stringify(post_data)).then(
+        function (response) {
+          that.btnPagar('c');
+          if(response.body.success){
+            Swal.fire("Correcto", response.body.message, "success");
+            if(response.body.url!='') window.location.href = response.body.url;
+          }else{
+            Swal.fire("Error", response.body.message, "error");
+          }
         },
-        function (err){
-            console.log(err);
+        function (err) {
+          this.btnPagar('c');
+          console.error(err);
+          Swal.fire("Error", err.body, "error");
         }
       );
     },
-    onError : function(response){
-      console.error(response);
+    onError: function (response) {
+      this.btnPagar('c');
+      Swal.fire("Error", response.data.description, "error");
+      console.error(response.data);
     },
     valideteForm: function () {
-      return true;
+      var validate = true;
+      if(this.plan==''){
+        validate = false
+      }
+      return validate;
     },
+    btnPagar: function (status) {
+      const that = this,
+            btnStatus = {
+              c: function() {
+                that.isDisabled = false;
+                that.btnText = 'Pagar';
+              },
+              p: function() {
+                that.isDisabled = true;
+                that.btnText = 'Pagando...';
+              }
+            };
+      btnStatus[status]();
+    },
+    descript: function(str){
+      switch(str) {
+      case '4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce':
+          return 3
+          break;
+      case 'e7f6c011776e8db7cd330b54174fd76f7d0216b612387a5ffcfb81e6f0919683':
+          return 6
+          break;
+      case '19581e27de7ced00ff1ce50b2047e7a567c76b1cbaebabe5ef03f7c3017bb5b7':
+          return 9
+          break;
+      case '6b51d431df5d7f141cbececcf79edf3dd861c3b4069f0b11661a3eefacbba918':
+          return 12
+          break;
+      }
+  }
   },
-  created: function () {},
+  created: function () {
+    const params = new URLSearchParams(location.search),
+        monto = params.get('mount'),
+        ref = params.get('ref'),
+        product = params.get('product'),
+        plans = params.get('plans'),
+        that = this;
+    if(monto==null||product==null||plans==null){
+      Swal.fire("Error", 'Parametros necesarios', "error");
+      that.btnPagar('c');
+    }else{
+      let planArr = plans.split(',');
+      planArr.forEach((plan, idx) => {
+          if (idx == 0) that.plan = that.descript(plan);
+          that.plans.push({text:that.descript(plan)+' Meses', value:that.descript(plan)});
+      });
+      that.productText = product;
+      that.mountText = '$'+monto;
+      that.mount = parseFloat(monto);
+    }
+  },
   mounted: function () {},
 });
 
@@ -58,5 +145,4 @@ $(document).ready(function () {
     "payment-form",
     "deviceIdHiddenFieldName"
   );
-  console.log(deviceSessionId);
 });
