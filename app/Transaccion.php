bded62c271e30b5dfefa7fc047eb5a526c5bcaf1
Ocejo <ocejojimenezjorgeluis@gmail.com>
2022-10-09 22:38:26 -0500

    se creo la validacion de pago

--- app/Transaccion.php
+++ app/Transaccion.php
@@ -2,33 +2,39 @@
 require_once '../vendor/autoload.php';
 use Openpay\Data\Openpay as Openpay;
 header('Content-Type: application/json');
+$data = json_decode(file_get_contents('php://input'), true);
 try {
     $openpay = Openpay::getInstance('mmey69zmnv0ub7zhny23', 'sk_1dc6b7d82dd24b8bac53bfaad6f9ec70');
-    $charge  = $openpay->charges->get('trdckaxemmphnrld0itm');
+    $charge  = $openpay->charges->get($data['id']);
     switch ($charge->status) {
         case 'failed':
             $message = json_encode([
-                'success' => false,
-                'message' => $charge->error_message,
-                'code'    => $charge->error_code
+                'success'  => false,
+                'message'  => "Lo sentimos " . $charge->customer->name . " Tu pago no ha sido completado, por favor verifique con su banco para poder realizar el pago.",
+                'code'     => $charge->error_code
             ]);
             break;
         case 'completed':
             $message = json_encode([
-                'success' => true,
-                'message' => 'Pago completado con ¡exito!',
-                'code'    => 200
+                'success'  => true,
+                'message'  => "¡Gracias " . $charge->customer->name . "! Tu pago ha sido procesado correctamente por un monto de $" . number_format($charge->amount, 2) . " MXN. Para continuar verifique su correo realizando los pasos siguientes. Si usted no ve este correo por favor verifique en su bandeja de correo no deseado.",
+                'code'     => 200
             ]);
             break;
         default:
             $message = json_encode([
-                'success' => false,
-                'message' => '',
-                'code'    => ''
+                'success'  => false,
+                'message'  => 'Pago aun en proceso...',
+                'code'     => 2811
             ]);
             break;
     }
     echo $message;
 } catch (\Throwable$th) {
-    throw $th;
+    http_response_code(400);
+    echo json_encode([
+        'success'  => false,
+        'message'  => 'Transacción Incorrecta',
+        'code'     => 400
+    ]);
 }
---
+++ js/success.js
@@ -0,0 +1,79 @@
+var app = new Vue({
+    el: "#app_page",
+    data: {
+        title:'PAGO ',
+        description:'',
+        src:'./images/',
+        loading: true,
+        contador:0,
+    },
+    methods: {
+        verificarPago:function(){
+            this.loadingFt();
+        },
+        loadingFt:function(){
+            this.contador++;
+            const that = this;
+            console.log(this.contador);
+            if (this.contador<4) {
+                var timerInterval;
+                const params = new URLSearchParams(location.search),
+                id_transaccion = params.get('id');
+                if(id_transaccion==''||id_transaccion==null){
+                    Swal.fire("Upss", 'Identificar no reconocido', "question");
+                    return true;
+                }
+                that.loading = true;
+                Swal.fire({
+                title: 'Verificando la Transacción',
+                html: 'Esto puede tardar unos minutos...',
+                timer: 5000,
+                timerProgressBar: false,
+                didOpen: () => {
+                    Swal.showLoading()
+                    const b = Swal.getHtmlContainer().querySelector('b')
+                    timerInterval = setInterval(() => {
+                    b.textContent = Swal.getTimerLeft()
+                    }, 100)
+                },
+                willClose: () => {
+                    let post_data = {
+                        id:id_transaccion
+                    }
+                    that.$http.post("./app/Transaccion.php", JSON.stringify(post_data)).then(
+                        function (res) {
+                            if(res.body.code == '2811') {
+                                that.loadingFt();
+                                return true
+                            }
+                            that.description = res.body.message;
+                            that.src += (res.body.success) ? 'true.svg' : 'false.svg';
+                            that.title += (res.body.success) ? 'EXITOSO' : 'DECLINADO';
+                        },
+                        function (err) {
+                            console.error(err);
+                            Swal.fire("Error", err.body.message, "error");
+                        }
+                    );
+                    that.loading = false;
+                    clearInterval(timerInterval);
+                }
+                }).then((result) => {
+                if (result.dismiss === Swal.DismissReason.timer) {
+                    console.log('I was closed by the timer')
+                }
+                });
+            }else{
+                that.description = 'Tu pago aun no ha sido procesado';
+                that.src += 'false.svg';
+                that.title += 'DECLINADO';
+            }
+            
+        }
+    },
+    created: function () {
+        this.verificarPago();
+    },
+
+  });
+  
